0a41f40f4764c5047fc2d14a504978b28f45bd76
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-03-28 13:00:42 -0600

    testing serverless functions

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server"
 
 let favorites: any = []
 
-export const GET = async () => {
-  return NextResponse.json({ favorites })
-}
+export const GET = async () => NextResponse.json({ favorites })
 
 export async function POST(request: Request) {
   const body = await request.json()
