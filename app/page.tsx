c6c030102d1a0e613b676df8dba46b6456073208
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-03-28 10:05:20 -0600

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import PokemonGrid from "@/components/PokemonGrid"
 
 export default async function Home() {
-  // const data = await getData()
-  // let clientPokedex: any = data
+  const data = await getData()
+  const dex = await pokedex(data)
+  console.log(dex)
 
   return (
     <div className='p-4 flex flex-col gap-2 antialiased h-screen w-screen'>
@@ -12,20 +13,24 @@ export default async function Home() {
   )
 }
 
-// const getData = async () => {
-//   const pokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${151}`)
-//   const pokeAPI_Response = await pokeAPI.json()
-//   const results = pokeAPI_Response.results
+const getData = async () => {
+  const pokeAPI = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${151}`)
+  const pokeAPI_Response = await pokeAPI.json()
+  const results = pokeAPI_Response.results
 
-//   if (!pokeAPI.ok) {
-//     throw new Error("Failed to fetch data")
-//   }
+  if (!pokeAPI.ok) {
+    throw new Error("Failed to fetch data")
+  }
 
-//   const pokedex = await results.map(async (poke: { name: any }) => {
-//     const myPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
-//     const res = await myPoke.json()
-//     return res
-//   })
+  return results
+}
 
-//   return Promise.all(pokedex)
-// }
+const pokedex = async (data: any) => {
+  const pokedex = data.map(async (poke: { name: any }) => {
+    const myPoke = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+    const res = await myPoke.json()
+    return res
+  })
+
+  return Promise.all(pokedex)
+}
