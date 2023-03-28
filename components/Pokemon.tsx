fe61fd7d6d670a34750d6db3e65158b3986cbf72
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-03-28 09:26:32 -0600

    test deploy

--- components/Pokemon.tsx
+++ components/Pokemon.tsx
@@ -36,6 +36,7 @@ const Pokemon = ({ poke }: any) => {
   }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
+
   // const getFavs = async () => {
   //   try {
   //     const data = await fetch("api/pokemon")
