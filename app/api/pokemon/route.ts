99246ea09a597ecf0c8f2790849e92e0285c1515
ayodyln <78442693+ayodyln@users.noreply.github.com>
2023-03-28 00:08:46 -0600

    test

--- app/api/pokemon/route.ts
+++ app/api/pokemon/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server"
 
 let favorites: any = []
 
-export const GET = async () => NextResponse.json({ favorites })
+export const GET = async () => {
+  return NextResponse.json({ favorites })
+}
 
 export async function POST(request: Request) {
   const body = await request.json()
